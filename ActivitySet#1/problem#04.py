7589631f088a03bed216138831715f7836e9ad63
Boddu Harshitha Harshitha <93128787+Bodduharshitha@users.noreply.github.com>
2022-05-15 04:06:32 +0000

    problem

--- ActivitySet#1/problem#04.py
+++ ActivitySet#1/problem#04.py
@@ -1,3 +1,12 @@
 # Conditional Execution
 
 hrs = input("Enter hours? ")
+
+hrs = input("Enter Hours:")
+h = float(hrs)
+xx = input("Enter the rate")
+x = float(xx)
+if h <= 40:
+   print(h * x)
+    elif h > 40:
+   print(40*x + (h-40)*1.5*x)
--- ActivitySet#1/problem#05.py
+++ ActivitySet#1/problem#05.py
@@ -10,3 +10,20 @@ rte = float(input("Enter rate per hour? "))
 
 p = computepay(hrs, rte)
 print("Pay", p)
+
+score = input("Enter Score: ")
+s =  float(score)
+x = 'Error'
+if s >= 0.9:
+	x = 'A'
+elif s >=0.8:
+	x='B'
+elif s >=0.7:
+	x='C'
+elif s >= 0.6:
+	x='D'
+elif s < .6:
+	x ='F'
+else:
+	x ="Out of Range"
+print (x)
