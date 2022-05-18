a183774526799230db30aaaeee02d90443c49f42
Boddu Harshitha Harshitha <93128787+Bodduharshitha@users.noreply.github.com>
2022-05-18 16:02:28 +0000

    09

--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -1,3 +1,14 @@
 # Lists
 
 filename = "dataset/romeo.txt"
+
+fname = input("Enter the file name:")
+fh = open(fname)
+r = fh.read()
+sp = r.split()
+lst = list()
+for w in sp:
+    if w not in lst:
+        lst.append(w)
+lst.sort()
+print(lst)
